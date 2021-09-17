7ff654116989f3d0f8348320dfcc8dc1655fb26e
Emanuel H. Farias <emanuelfarias@outlook.com>
2021-09-16 22:56:43 -0300

    stream template on form POST

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,5 +3,10 @@ class ContactsController < ApplicationController
   end
 
   def create
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.append(:messages, partial: 'create')
+      end
+    end
   end
 end
